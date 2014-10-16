8bcde2f0e041b808688826e0ed4e05e31e4b553d
simensta <a.simenstad@gmail.com>
2014-10-16 17:27:42 -0500

    can make api calls

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,12 @@ class Item < ActiveRecord::Base
   belongs_to :pantry
 
   validates :name, :location, :expiration_date, :pantry_id, :prototype_id, presence: true
+
+  before_save do
+    proto_item = Prototype.find_by_name("#{self.name}").shelf_life
+    puts proto_item
+
+    self.expiration_date = Time.now + (proto_item).day
+  end
+
 end
