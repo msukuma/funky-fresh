08f5c419eb50cb1cab882c4b714312aa051b4986
Nigel Dunetts <ndunetts@gmail.com>
2014-10-16 16:52:09 -0500

    controllers and views in progress, prepping to merge with user branch

--- app/controllers/pantries_controller.rb
+++ app/controllers/pantries_controller.rb
@@ -2,17 +2,23 @@ class PantriesController < ApplicationController
 
 	def index
 		# show all of the pantries for user
+		# @user = User.find(params[current_user.id])
+		@user
+		# @pantries = @user.pantries
+		render 'index'
 	end
 
 	def new
+		@user
 		@pantry = Pantry.new
 	end
 
 	def create
+		@user
 		@pantry = Pantry.new(pantry_params)
 		if @pantry.save
-			@pantry_participation = PantryParticipation.create(user_id: current_user.id, pantry_id: @pantry.id)
-				redirect_to user_pantry_index_path
+			@pantry_participation = PantryParticipation.create(user_id: @user.id, pantry_id: @pantry.id)
+				redirect_to user_pantries_path
 		else
 			redirect_to new_user_pantry_path
 		end
@@ -29,13 +35,13 @@ class PantriesController < ApplicationController
 	def update
 		@pantry = Pantry.find(params[:id])
 		@pantry.update(pantry_params)
-		redirect_to user_pantry_index_path
+		redirect_to user_pantries_path
 	end
 
 	def destroy
 		@pantry = Pantry.find(params[:id])
 		@pantry.destroy
-		redirect_to user_pantry_index_path
+		redirect_to user_pantries_path
 	end
 
 	private
