d4b94822dec7f829079b0b772b1a9e4550ed0460
John Park <just.johnpark@gmail.com>
2014-10-16 12:59:43 -0500

    everyone pull this

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ActiveRecord::Base
   has_many :pantry_participations
   has_many :pantries, through: :pantry_participations
-  # has_many :original_pantries, foreign_key: :creator_id, class_name: "Pantry"
+  has_many :original_pantries, foreign_key: :creator_id, class_name: "Pantry"
 
   validates :first_name, :last_name, :email, :password_digest, presence: true
-  # validates :email, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/, on: :create }
+  validates :email, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/, on: :create }
   validates :password, :length => {:minimum => 6}
 
   has_secure_password
