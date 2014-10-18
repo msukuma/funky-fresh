a669909da8d61637c2974332d18525cacf97516f
Nixon Lolila Ramin <nixonlr@gmail.com>
2014-10-18 15:45:40 -0500

    client side validations complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,8 +17,8 @@ class UsersController < ApplicationController
   end
 
 	def create
-		@user = User.create(user_params)
-    if @user
+		@user = User.new(user_params)
+    if @user.save
     	session[:user_id] = @user.id
       redirect_to user_path(@user)
     else
@@ -66,7 +66,7 @@ class UsersController < ApplicationController
 		private
 
 	def user_params
-		params.require(:user).permit(:first_name, :last_name, :email, :password)
+		params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
 	end
 
 	# def find_user
