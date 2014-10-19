6bafc882930a3d99677bc2f8a4d6959dfef7bfed
John Park <just.johnpark@gmail.com>
2014-10-18 21:50:58 -0500

    another update for nigel and andrea

--- app/assets/javascripts/item.js
+++ app/assets/javascripts/item.js
@@ -7,27 +7,8 @@ $(function(){
   });
 
   $(document).on("ajax:complete", "div.new-item-create form", function(event, data){
-    console.log("I am in the create")
-    console.log("adding items");
-    console.log(data.responseText);
-    $("#item-block").html(data.responseText);
-  })
- });
-
-
-
-
-
-  // $(".item-delete").on("ajax:complete", ".enable-delete", function(event, data, status, xhr){
-  //     console.log(event.target)
-  //     var item = $(event.target).closest("tr");
-  //     console.log(item);
-  //     item.remove();
-  //  });
-
-  // $("div.new-item-create form").on("ajax:complete", function(event, data, status, xhr){
-  //     console.log(event.target);
-  //     console.log(data.responseText)
-  //     $(".item-row").hide();
-  //     $("#item-block").append(data.responseText);
-  //  });
+    // console.log($(event.target))
+    semiTarget = $(this).parent().prev();
+    semiTarget.html(data.responseText);
+  });
+});
