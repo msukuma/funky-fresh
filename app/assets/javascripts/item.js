f3b729073a9b98d3759b44f0056a5263044e14f0
simensta <a.simenstad@gmail.com>
2014-10-18 19:24:35 -0500

    fixed js issue with ajax

--- app/assets/javascripts/item.js
+++ app/assets/javascripts/item.js
@@ -14,21 +14,3 @@ $(function(){
     $("#item-block").html(data.responseText);
   })
  });
-
-
-
-
-
-  // $(".item-delete").on("ajax:complete", ".enable-delete", function(event, data, status, xhr){
-  //     console.log(event.target)
-  //     var item = $(event.target).closest("tr");
-  //     console.log(item);
-  //     item.remove();
-  //  });
-
-  // $("div.new-item-create form").on("ajax:complete", function(event, data, status, xhr){
-  //     console.log(event.target);
-  //     console.log(data.responseText)
-  //     $(".item-row").hide();
-  //     $("#item-block").append(data.responseText);
-  //  });
