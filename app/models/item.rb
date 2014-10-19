6667f0b1c42b58f4c564e33fe76fda8ae106aef6
Nixon Lolila Ramin <nixonlr@gmail.com>
2014-10-18 19:13:29 -0500

    about to pull

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,7 +4,7 @@ class Item < ActiveRecord::Base
   belongs_to :prototype
   belongs_to :pantry
 
-  validates :pantry_id, :prototype_id, :expiration_date, presence: true
+  validates :pantry_id, :prototype_id, presence: true
 
   before_create :set_expiration_date, if: -> { self.expiration_date.blank? }
   after_create :set_prototype_shelf_life, if: -> { self.prototype.shelf_life.blank? }
@@ -23,7 +23,6 @@ class Item < ActiveRecord::Base
   end
 
   def set_expiration_date
-    # self.expiration_date = self.prototype.expiration_date.days_from_now
     self.expiration_date = Time.now + (self.prototype.shelf_life).days
   end
 
