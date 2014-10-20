8030657cf5f0082361b40eec7fff94c49ce5b8e0
simensta <a.simenstad@gmail.com>
2014-10-20 15:45:27 -0500

    changed csv infinity inputs to 20yrs or 7304 days

--- spec/models/invite_spec.rb
+++ spec/models/invite_spec.rb
@@ -1,5 +1,37 @@
 require 'spec_helper'
 
 describe Invite do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:invite) { Invite.new }
+  context 'validations' do
+    it "should have a name" do
+      expect(invite).to have_db_column :email
+      expect(invite).to have_db_column :pantry_id
+      expect(invite).to have_db_column :sender_id
+      expect(invite).to have_db_column :recipient_id
+      expect(invite).to have_db_column :token
+    end
+
+  it 'should validate presence of name' do
+      expect(item).to validate_presence_of :email
+  end
+
+  it 'should validate presence of name' do
+      expect(item).to validate_presence_of :pantry_id
+  end
+
+  it 'should validate presence of name' do
+      expect(item).to validate_presence_of :sender_id
+  end
+
+
+  it 'should validate presence of name' do
+      expect(item).to validate_presence_of :token
+  end
+
+  context 'associations' do
+    it { should belong_to(:pantry) }
+    it { should belong_to(:sender) }
+    it { should belong_to(:recipient) }
+  end
+
 end
