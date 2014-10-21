b7cfb38af25f737c036a2b4d3d9a5e248b6f7a68
Nigel Dunetts <ndunetts@gmail.com>
2014-10-20 20:50:22 -0500

    adjusted prod email settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,7 +82,7 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'http://funky-fresh.herokuapp.com' }
 
-  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: "587",
