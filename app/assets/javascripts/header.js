a07c204409f00c89654e1f6ec9545d8f97b1332e
Nixon Lolila Ramin <nixonlr@gmail.com>
2014-10-22 17:03:06 -0500

    Tipsy half functional

--- app/assets/javascripts/header.js
+++ app/assets/javascripts/header.js
@@ -10,4 +10,20 @@ $(document).ready(function() {
   $(document).on("ajax:complete", "header li a.home", function(event,data){
     $(".body_container").html(data.responseText);
   });
+
+  $('a[rel=tipsy]').tipsy({fade: true, gravity: 'n'});
+
+  $('#home').tipsy({fallback: "Home"});
+  $('#sign_up').tipsy({fallback: "Sign Up"});
+  $('#login').tipsy({fallback: "Login"});
+  $('#logout_button').tipsy({fallback: "Logout"});
+  $('#kitchen').tipsy({fallback: "Pantries"});
+  $('#edit_cog').tipsy({fallback: "Edit Profile"});
+  $("[id^='edit_pantry']").tipsy({fallback: "Change Pantry Name"})
+  $("[id^='new_item']").tipsy({fallback: "Add Item"})
+  $("[id^='new_invite']").tipsy({fallback: "Invite to Pantry"})
+  $(".cookbook").tipsy({fallback: "Find Recipes"})
+
+
+
 });
