ab03f105cd7816501b6036716d35ecf92a72f927
Nixon Lolila Ramin <nixonlr@gmail.com>
2014-10-23 21:44:40 -0500

    email working with recipes

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,4 +1,6 @@
 class UserMailer < ActionMailer::Base
+  include RecipeHelper
+
   default from: "Funky||Fresh <no-reply@funky||fresh.com>"
 
   def welcome_email(user)
@@ -11,10 +13,23 @@ class UserMailer < ActionMailer::Base
   def expiration_alert_email(user)
     @user_pantries = {}
     @user = user
-    @user.all_pantries.each do |pantry|
-      @user_pantries[pantry.name] = pantry.item_checker(5) if pantry.item_checker(5).length != 0
+    pantries = @user.all_pantries
+    
+    pantries.each do |pantry|
+      @user_pantries[pantry.name] = {"items" => pantry.item_checker(5)} if pantry.item_checker(5).length != 0
     end
-    @user_pantries
+
+    @user_pantries.each do |pantry_name, value|
+        
+      pantries.each do |pantry|
+        if pantry.name == pantry_name
+          ingredients = value['items'].map{|item| item.prototype.name}.join(', ')
+          search_results = search_yummly_for(ingredients)
+          value['matches'] = best_matches(search_results, pantry)
+        end
+      end
+    end
+    
     if @user_pantries.length != 0
       mail(to: @user.email, subject: "Food expiration alert from your friends at Funky||Fresh")
     end
